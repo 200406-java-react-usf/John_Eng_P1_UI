a02b3acda87fcca8319eaeeb82ef31a77e715b1c
john1eng <john1eng@hotmail.com>
2020-05-22 07:25:30 -0400

    change to /emplReimbs in update-empl

--- src/remote/update-empl.ts
+++ src/remote/update-empl.ts
@@ -2,7 +2,7 @@ import { ERSreimb } from "../models/ers-reimb";
 import {devboardsClient } from "./devboards-client";
 
 export async function updateReimb(REIMB_ID: number, AMOUNT: number, RECEIPT: string, DESCRIPTION: string, REIMB_TYPE: string): Promise<ERSreimb> {
-    let response = await devboardsClient.patch('/emplReimb', {REIMB_ID, AMOUNT, RECEIPT, DESCRIPTION, REIMB_TYPE});
+    let response = await devboardsClient.patch('/emplReimbs', {REIMB_ID, AMOUNT, RECEIPT, DESCRIPTION, REIMB_TYPE});
     console.log('from updateUser()', response)
     return await response.data;
 }
